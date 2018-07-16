af59a96c3536617166145252979d3e2ea2c50f65
GBTC441004UR <GBTC441004UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-16 11:09:53 -0400

    added random

--- src/weekone.java
+++ src/weekone.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class weekone {
@@ -6,9 +7,13 @@ public class weekone {
 
         Scanner planner = new Scanner (System.in);
 
+        Random rando = new Random();
+
         String itemName01, itemName02;
         Double itemPrice01, itemPrice02;
         int itemQuantity01, itemQuantity02;
+
+        itemQuantity01 = 1 + rando.nextInt(10);
 
         System.out.println("Enter name of your first item: ");
         itemName01 = planner.next();
@@ -16,8 +21,8 @@ public class weekone {
         System.out.println("Whats the price? ");
         itemPrice01 = planner.nextDouble();
 
-        System.out.println("How many do you have? ");
-        itemQuantity01 = planner.nextInt();
+        //System.out.println("How many do you have? ");
+        //itemQuantity01 = planner.nextInt();
 
         System.out.println("Enter name of your second item: ");
         itemName02 = planner.next();
@@ -31,21 +36,22 @@ public class weekone {
         double Cost1 = itemPrice01 + itemQuantity01;
         double Cost2 = itemPrice02 + itemQuantity02;
 
-        System.out.println("Item Name       Price      Quantity     Cost");
+        System.out.println("Item Name\tPrice\tQuantity\tCost");
 
-        System.out.println(itemName01 + "       $" + itemPrice01 + "        " + itemQuantity01 + "      " + Cost1);
+        System.out.println(itemName01 + "\t\t$" + itemPrice01 + "\t\t" + itemQuantity01 + "\t\t$" + Cost1);
 
 
-        System.out.println(itemName02 + "       $" + itemPrice02 + "        " + itemQuantity02 + "      " + Cost2);
+        System.out.println(itemName02 + "\t\t$" + itemPrice02 + "\t\t" + itemQuantity02 + "\t\t$" + Cost2);
 
 
         System.out.println( "Total purchase amount: $" + ( Cost1 + Cost2));
 
         if (Cost1 + Cost2 > 20) {
-            System.out.println( "You spent too much today");
+            System.out.println( "You spent too much today");}
+            else {System.out.println("Thank you for shopping at my market!");
         }
 
-        System.out.println("Thank you for shopping at my market!");
+
     }
 
 }
